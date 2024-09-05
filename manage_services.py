96340e5f706d2c07719de561bb590b993941f4de
Ronan <ronanr2003@gmail.com>
2024-09-05 20:50:12 +0200

    Update manage_services.py

--- manage_services.py
+++ manage_services.py
@@ -19,14 +19,12 @@ services = [
     "OracleVssWriterORCL"
 ]
 
-
 def is_admin():
     """Check if the script is being run with administrative privileges."""
     try:
         return ctypes.windll.shell32.IsUserAnAdmin()
     except:
         return False
-
 
 def manage_services(action):
     if action not in ["start", "stop"]:
@@ -44,15 +42,12 @@ def manage_services(action):
         except Exception as ex:
             print(f"An unexpected error occurred while trying to {action} {service_name}: {ex}")
 
-
 def main():
     action = input("Enter 'start' to start all services or 'stop' to stop all services: ").lower()
     manage_services(action)
-
 
 if __name__ == "__main__":
     if is_admin():
         main()
     else:
         print("Please run this script with administrative privileges.")
-        # You can also add a prompt to elevate the script to admin rights if necessary.
